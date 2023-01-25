eb825660a564d34e2c992748e43541380d4f82be
Kirill Lavrov <lkv@myttk.ru>
2023-01-25 21:34:13 +0700

    api address changed

--- src/api/const.js
+++ src/api/const.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://api-c-money.herokuapp.com';
-export const API_WS = 'wss://api-c-money.herokuapp.com/currency-feed';
+export const API_URL = 'https://field-pleasant-subway.glitch.me';
+export const API_WS = 'wss://field-pleasant-subway.glitch.me/currency-feed';
